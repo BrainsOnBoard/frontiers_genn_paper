fb1b0e1ee9b00fc6289becff5a767bbf7fed008e
jamie <neworderofjamie@gmail.com>
2018-08-21 15:59:17 +0100

    Robust to some failed digits, restrict character set to decimals

--- scripts/read_lcd.py
+++ scripts/read_lcd.py
@@ -1,12 +1,12 @@
 import csv
 import cv2
 import time
-from subprocess import call, check_output
+from subprocess import call, check_output, CalledProcessError
 from tempfile import NamedTemporaryFile
 cap = cv2.VideoCapture(0)
 
 start_time = time.time()
-with open("power.csv", 'wb') as csv_file, NamedTemporaryFile(suffix=".png") as tmp_file:
+with open("power.csv", 'wb') as csv_file, NamedTemporaryFile(suffix=".png", delete=False) as tmp_file:
     csv_writer = csv.writer(csv_file, delimiter=',')
     csv_writer.writerow(["Time [s]", "Power [W]"])
     while True:
@@ -15,7 +15,14 @@ with open("power.csv", 'wb') as csv_file, NamedTemporaryFile(suffix=".png") as t
             digit = frame[1][350:395,245:345:]
             cv2.imwrite(tmp_file.name, digit)
 
-            output = check_output(["ssocr", "--number-digits=-1", tmp_file.name])
-            power = float(output)
-            print("%fW" % power)
-            csv_writer.writerow([time.time() - start_time, power])
+            try:
+                output = check_output(["ssocr", "--charset=decimal", "--number-digits=-1", tmp_file.name])
+                power = float(output)
+                print("%fW" % power)
+                csv_writer.writerow([time.time() - start_time, power])
+            except CalledProcessError as err:
+                if err.returncode == 2:
+                    print("Unable to recognise digit")
+                    pass
+                else:
+                    raise
