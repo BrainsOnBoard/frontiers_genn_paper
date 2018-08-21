a6f41e99bf03d0a03524239f25d22db93c844819
jamie <neworderofjamie@gmail.com>
2018-08-21 16:27:11 +0100

    Robustness to other stupid output

--- scripts/read_lcd.py
+++ scripts/read_lcd.py
@@ -12,17 +12,21 @@ with open("power.csv", 'wb') as csv_file, NamedTemporaryFile(suffix=".png", dele
     while True:
         frame = cap.read()
         if frame[0]:
-            digit = frame[1][350:395,245:345:]
+            digit = frame[1][350:395,245:340:]
             cv2.imwrite(tmp_file.name, digit)
 
             try:
                 output = check_output(["ssocr", "--charset=decimal", "--number-digits=-1", tmp_file.name])
-                power = float(output)
-                print("%fW" % power)
-                csv_writer.writerow([time.time() - start_time, power])
+                if output.count(".") == 1 and output.count("-") == 0:
+                    power = float(output)
+                    print("%fW" % power)
+                    csv_writer.writerow([time.time() - start_time, power])
+                else:
+                    print("Parser output invalid: '%s'" % output)
             except CalledProcessError as err:
                 if err.returncode == 2:
-                    print("Unable to recognise digit")
+                    print("Unable to recognise digit '%s'" % err.output)
                     pass
                 else:
+                    print err.returncode
                     raise
