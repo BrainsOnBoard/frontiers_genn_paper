0988491713eb542961a80ebe64a370b46d7417c7
jamie <neworderofjamie@gmail.com>
2018-08-22 16:27:19 +0100

    STDP performance figure

--- scripts/plot_performance.py
+++ scripts/plot_performance.py
@@ -14,7 +14,11 @@ def plot(data, filename, num_ref, real_time_s=None):
     neuron_sim_time = np.asarray(columns[2],  dtype=float) / 1000.0
     synapse_sim_time = np.asarray(columns[3],  dtype=float) / 1000.0
 
+    post_learn_sim_time = None if (len(columns) < 5) else np.asarray(columns[4],  dtype=float) / 1000.0
+
     overhead = total_sim_time - neuron_sim_time - synapse_sim_time
+    if post_learn_sim_time is not None:
+        overhead -= post_learn_sim_time
 
     fig, axis = plt.subplots(figsize=(plot_settings.column_width, 90.0 * plot_settings.mm_to_inches),
                             frameon=False)
@@ -33,6 +37,11 @@ def plot(data, filename, num_ref, real_time_s=None):
     offset += neuron_sim_time[gpu_bar_x_slice]
     synapse_sim_actor = axis.bar(bar_x[gpu_bar_x_slice], synapse_sim_time[gpu_bar_x_slice], bar_width, offset)[0]
     offset += synapse_sim_time[gpu_bar_x_slice]
+
+    if post_learn_sim_time is not None:
+        post_learn_sim_actor = axis.bar(bar_x[gpu_bar_x_slice], post_learn_sim_time[gpu_bar_x_slice], bar_width, offset)[0]
+        offset += post_learn_sim_time[gpu_bar_x_slice]
+
     overhead_actor = axis.bar(bar_x[gpu_bar_x_slice], overhead[gpu_bar_x_slice], bar_width, offset)
     offset += overhead[gpu_bar_x_slice]
 
@@ -62,13 +71,18 @@ def plot(data, filename, num_ref, real_time_s=None):
     axis.set_xticks(bar_x)
     axis.set_xticklabels(device, rotation="vertical", ha="center", multialignment="right")
 
-    fig.legend([neuron_sim_actor, synapse_sim_actor, overhead_actor],
-            ["Neuron\nsimulation", "Synapse\nsimulation", "Overhead"],
-            ncol=3, loc="lower center")
+    if post_learn_sim_time is not None:
+        fig.legend([neuron_sim_actor, synapse_sim_actor, post_learn_sim_actor, overhead_actor],
+                ["Neuron simulation", "Synapse simulation", "Postsynaptic learning", "Overhead"],
+                ncol=2, loc="lower center")
+    else:
+        fig.legend([neuron_sim_actor, synapse_sim_actor, overhead_actor],
+                ["Neuron simulation", "Synapse\nsimulation", "Overhead"],
+                ncol=2, loc="lower center")
 
     # Set tight layour - tweaking bottom to fit in axis
     # text and right to fit in right break marker
-    fig.tight_layout(pad=0, rect=(0.0, 0.09, 1.0, 0.96))
+    fig.tight_layout(pad=0, rect=(0.0, 0.15, 1.0, 0.96))
     fig.savefig(filename)
 
 # Total simulation time, neuron simulation, synapse simulation
@@ -79,9 +93,10 @@ microcircuit_data = [("Jetson TX2", 258751, 99775.9, 155475),
                      ("HPC\n(fastest)", 24296.0, 0.0, 0.0),
                      ("SpiNNaker", 200000, 0.0, 0.0)]
 
-stdp_data = [("Tesla K40m\nBitmask", 4736610, 435387, 296357),
-             ("Tesla V100\nBitmask", 564826, 100144, 82951.6),
-             ("Tesla V100\nRagged", 567267, 99346.3, 85975.4)]
+# Total simulation time, neuron simulation, synapse simulation, postsynaptic learning
+stdp_data = [("Tesla K40m\nBitmask", 4736610, 435387, 296357, 3925070),
+             ("Tesla V100\nBitmask", 564826, 100144, 82951.6, 307273),
+             ("Tesla V100\nRagged", 567267, 99346.3, 85975.4, 307433)]
 
 plot(microcircuit_data, "../figures/microcircuit_performance.eps", 2, 10.0)
 plot(stdp_data, "../figures/stdp_performance.eps", 0, 200.0)
