4d58c3af21c3859b430c49da45e7dc3a58bdc940
jamie <neworderofjamie@gmail.com>
2018-08-28 15:28:20 +0100

    More generalising

--- scripts/plot_performance.py
+++ scripts/plot_performance.py
@@ -6,17 +6,20 @@ import sys
 import plot_settings
 import utils
 
-def plot(data, filename, num_ref, calc_overhead, real_time_s=None):
+def plot(data, filename, num_ref, calc_overhead, legend_text, real_time_s=None):
     columns = zip(*data)
     device = np.asarray(columns[0],  dtype=str)
 
+    # Read times into numpy arrays
     num_time_columns = len(columns) - 1
     times = np.empty((num_time_columns, len(device)), dtype=float)
     for i, col in enumerate(columns[1:]):
         times[i,:] = col
 
+    # Convert ms to s
     times /= 1000.0
 
+    # If overheads are being calculated, subtract all preceeding rows from last row
     if calc_overhead:
         for i in range(times.shape[0] - 1):
             times[-1,:-num_ref] -= times[i,:-num_ref]
@@ -34,20 +37,14 @@ def plot(data, filename, num_ref, calc_overhead, real_time_s=None):
     # Plot stacked, GPU bars
     gpu_bar_x_slice = np.s_[:] if num_ref == 0 else np.s_[:-num_ref]
 
-    actors = []
+    legend_actors = []
     for i in range(times.shape[0]):
-        actors.append(axis.bar(bar_x[gpu_bar_x_slice], times[i,gpu_bar_x_slice], bar_width, offset)[0])
+        legend_actors.append(axis.bar(bar_x[gpu_bar_x_slice], times[i,gpu_bar_x_slice], bar_width, offset)[0])
         offset += times[i,gpu_bar_x_slice]
 
     # Plot individual other bars
     if num_ref > 0:
         axis.bar(bar_x[-num_ref:], times[-1,-num_ref:], bar_width, 0.0)
-
-    # Add real-timeness annoation
-    #for t, x in zip(total_sim_time, bar_x):
-    #    axis.text(x, t,
-    #              "%.2f$\\times$\nreal-time" % (1.0 / t),
-    #              ha="center", va="bottom", )
 
     axis.set_ylabel("Time [s]")
 
@@ -61,18 +58,12 @@ def plot(data, filename, num_ref, calc_overhead, real_time_s=None):
     # Remove vertical grid
     axis.xaxis.grid(False)
 
-    # Add x ticks labelling delay type
+    # Add x ticks
     axis.set_xticks(bar_x)
     axis.set_xticklabels(device, rotation="vertical", ha="center", multialignment="right")
 
-    if num_time_columns == 4:
-        fig.legend(actors,
-                ["Neuron simulation", "Synapse simulation", "Postsynaptic learning", "Overhead"],
-                ncol=2, loc="lower center")
-    else:
-        fig.legend(actors,
-                ["Neuron simulation", "Synapse\nsimulation", "Overhead"],
-                ncol=2, loc="lower center")
+    # Add legend
+    fig.legend(legend_actors, legend_text, ncol=2, loc="lower center")
 
     # Set tight layour - tweaking bottom to fit in axis
     # text and right to fit in right break marker
@@ -92,6 +83,8 @@ stdp_data = [("Tesla K40m\nBitmask", 435387, 296357, 3925070, 4736610),
              ("Tesla V100\nBitmask", 100144, 82951.6, 307273, 564826),
              ("Tesla V100\nRagged", 99346.3, 85975.4, 307433, 567267)]
 
-plot(microcircuit_data, "../figures/microcircuit_performance.eps", 2, True, 10.0)
-plot(stdp_data, "../figures/stdp_performance.eps", 0, True, 200.0)
+plot(microcircuit_data, "../figures/microcircuit_performance.eps", 2, True,
+     ["Neuron simulation", "Synapse\nsimulation", "Overhead"], 10.0)
+plot(stdp_data, "../figures/stdp_performance.eps", 0, True,
+     ["Neuron simulation", "Synapse simulation", "Postsynaptic learning", "Overhead"], 200.0)
 plt.show()
