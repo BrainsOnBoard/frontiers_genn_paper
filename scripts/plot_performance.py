992dc404474e0f7bba5cd0ec6a0bde11aa16742f
jamie <neworderofjamie@gmail.com>
2018-08-29 11:01:10 +0100

    Stacked bars on a log scale! are you insane!?

--- scripts/plot_performance.py
+++ scripts/plot_performance.py
@@ -32,8 +32,37 @@ def plot(data, filename, num_ref, calc_overhead, legend_text, real_time_s=None, 
 
     # Correctly place bars
     bar_width = 0.8
-    bar_pad = 0.4
-    bar_x = np.arange(0.0, len(device) * (bar_width + bar_pad), bar_width + bar_pad)
+
+
+    # If there are no groups space bars evenly
+    if group_size is None:
+        bar_pad = 0.4
+        bar_x = np.arange(0.0, len(device) * (bar_width + bar_pad), bar_width + bar_pad)
+    # Otherwise
+    else:
+        bar_pad = 0.1
+        group_pad = 0.75
+        start = 0.0
+        group_x = []
+        bar_x = np.empty(len(device))
+
+        # Calculate bar positions of grouped GPU bars
+        for d in range(0, len(device) - num_ref, group_size):
+            end = start + ((bar_width + bar_pad) * group_size)
+            bar_x[d:d + group_size] = np.arange(start, end, bar_width + bar_pad)
+
+            group_x.append(start + ((end - bar_width - start) * 0.5))
+
+            # Update start for next group
+            start = end + group_pad
+
+        # Calculate bar positions of other bars
+        for d in range(len(device) - num_ref, len(device)):
+            bar_x[d] = start
+            group_x.append(start)# + (bar_width * 0.5))
+
+            # Update start for next group
+            start += (bar_width + group_pad)
 
     offset = np.zeros(len(bar_x) - num_ref)
 
@@ -70,11 +99,15 @@ def plot(data, filename, num_ref, calc_overhead, legend_text, real_time_s=None, 
         axis.set_yscale("log", nonposy="clip")
 
     # Add legend
-    fig.legend(legend_actors, legend_text, ncol=2, loc="lower center")
+    if legend_text is not None:
+        fig.legend(legend_actors, legend_text, ncol=2, loc="lower center")
 
-    # Set tight layour - tweaking bottom to fit in axis
-    # text and right to fit in right break marker
-    fig.tight_layout(pad=0, rect=(0.0, 0.15, 1.0, 0.96))
+        # Set tight layour - tweaking bottom to fit in axis
+        # text and right to fit in right break marker
+        fig.tight_layout(pad=0, rect=(0.0, 0.15, 1.0, 0.96))
+    else:
+        fig.tight_layout(pad=0)
+
     fig.savefig(filename)
 
 # Total simulation time, neuron simulation, synapse simulation
@@ -85,16 +118,16 @@ microcircuit_data = [("Jetson TX2", 99570.4, 155284, 258350),
                      ("HPC\n(fastest)", 0.0, 0.0, 24296.0),
                      ("SpiNNaker", 0.0, 0.0, 200000)]
 
-microcircuit_init_data = [("Jetson TX2", "Device\ninit", 753.284 + 950.965, 1683.32),
-                          ("Jetson TX2", "Host\ninit", 125.569, 14.438 + 541196 + 85984.6),
-                          ("GeForce 1050ti", "Device\ninit", 347.681 + 499.292, 561.601),
-                          ("GeForce 1050ti", "Host\ninit", 362.013, 7.14622 + 19110 + 49768.2),
-                          ("Tesla K40c", "Device\ninit", 204.258 + 361.698, 392.913),
-                          ("Tesla K40c", "Host\ninit", 0.0, 18522.8),
-                          ("Tesla V100", "Device\ninit", 58.6588 + 142.279, 445.239),
-                          ("Tesla V100", "Host\ninit", 0.0, 16182.2),
-                          ("HPC\n(fastest)", "", 0.0, 2000.0),
-                          ("SpiNNaker", "", 0.0, 10.0 * 60.0 * 60.0 * 1000.0)]
+microcircuit_init_data = [("Jetson TX2", "Device\ninit", 753.284 + 950.965 + 1683.32),
+                          ("Jetson TX2", "Host\ninit", 125.569 + 14.438 + 541196 + 85984.6),
+                          ("GeForce 1050ti", "Device\ninit", 347.681 + 499.292 + 561.601),
+                          ("GeForce 1050ti", "Host\ninit", 362.013 + 7.14622 + 19110 + 49768.2),
+                          ("Tesla K40c", "Device\ninit", 204.258 + 361.698 + 392.913),
+                          ("Tesla K40c", "Host\ninit", 18522.8),
+                          ("Tesla V100", "Device\ninit", 58.6588 + 142.279 + 445.239),
+                          ("Tesla V100", "Host\ninit", 16182.2),
+                          ("HPC\n(fastest)", "", 2000.0),
+                          ("SpiNNaker", "", 10.0 * 60.0 * 60.0 * 1000.0)]
 
 # Total simulation time, neuron simulation, synapse simulation, postsynaptic learning
 stdp_data = [("Tesla K40m\nBitmask", 435387, 296357, 3925070, 4736610),
@@ -102,7 +135,7 @@ stdp_data = [("Tesla K40m\nBitmask", 435387, 296357, 3925070, 4736610),
              ("Tesla V100\nRagged", 99346.3, 85975.4, 307433, 567267)]
 
 plot(microcircuit_init_data, "../figures/microcircuit_init_performance.eps", 2, False,
-     ["Device initialisation", "Host initialisation"], None, 2, True)
+     None, None, 2, True)
 
 plot(microcircuit_data, "../figures/microcircuit_performance.eps", 2, True,
      ["Neuron simulation", "Synapse\nsimulation", "Overhead"], 10.0)
