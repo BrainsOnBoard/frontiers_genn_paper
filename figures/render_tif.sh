729e1f6b1aef9f74c7e411b6dd968b5f3992c6e9
jamie <neworderofjamie@gmail.com>
2018-09-27 11:46:08 +0100

    TIFF figures for submission

--- figures/render_tif.sh
+++ figures/render_tif.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for f in *pdf; do
-  gs -dSAFER -dBATCH -dNOPAUSE -r1200 -sDEVICE=tiff24nc -sCompression=lzw -dEPSCrop -sOutputFile="${f%.pdf}.tif" "$f"
+for f in *.eps; do
+  gs -dSAFER -dBATCH -dNOPAUSE -r1200 -sDEVICE=tiff24nc -sCompression=lzw -dEPSCrop -sOutputFile="${f%.eps}.tif" "$f"
 done
