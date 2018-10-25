afe874b700122748421f828b157fb62beb391376
jamie <neworderofjamie@gmail.com>
2018-10-25 14:10:35 +0100

    Added Jetson scaling data

--- scripts/plot_scaling.py
+++ scripts/plot_scaling.py
@@ -8,7 +8,8 @@ import utils
 
 scales = np.asarray([1.0, 0.75, 0.5, 0.25])
 data = [("Tesla K40c", np.asarray([41911.5, 33950.1, 26541.2, 20955.2])),
-        ("Tesla V100", np.asarray([21645.4,20156, 17562, 15512.3]))]
+        ("Tesla V100", np.asarray([21645.4,20156, 17562, 15512.3])),
+        ("Jetson TX2", np.asarray([258350, 180802, 110047, 51710.6]))]
 
 fig, axis = plt.subplots(figsize=(plot_settings.column_width, 90.0 * plot_settings.mm_to_inches),
                          frameon=False)
