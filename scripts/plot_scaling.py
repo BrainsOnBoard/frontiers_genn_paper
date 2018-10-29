ac8de6bd3b6492a56b9940437ac75cab7bc0cf19
jamie <neworderofjamie@gmail.com>
2018-10-29 09:55:34 +0000

    Added GeForce 1050ti to scaling plot

--- scripts/plot_scaling.py
+++ scripts/plot_scaling.py
@@ -9,6 +9,7 @@ import utils
 scales = np.asarray([1.0, 0.75, 0.5, 0.25])
 data = [("Tesla K40c", np.asarray([41911.5, 33950.1, 26541.2, 20955.2])),
         ("Tesla V100", np.asarray([21645.4,20156, 17562, 15512.3])),
+        ("GeForce 1050ti", np.asarray([137592, 119623, 102452, 87691.1])),
         ("Jetson TX2", np.asarray([258350, 180802, 110047, 51710.6]))]
 
 fig, axis = plt.subplots(figsize=(plot_settings.column_width, 90.0 * plot_settings.mm_to_inches),
@@ -21,8 +22,8 @@ axis.set_ylabel("Time [s]")
 axis.axhline(10.0, color="black", linestyle="--")
 
 fig.legend(actors, [n for n, _ in data],
-           loc="lower center", ncol=len(data))
-fig.tight_layout(pad=0, rect=[0.0, 0.1, 1.0, 1.0])
+           loc="lower center", ncol=2)
+fig.tight_layout(pad=0, rect=[0.0, 0.15, 1.0, 1.0])
 if not plot_settings.presentation:
     fig.savefig("../figures/microcircuit_scaling.eps")
 plt.show()
